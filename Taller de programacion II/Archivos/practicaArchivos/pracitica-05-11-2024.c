c331510eb0d08e0955d872299164d802731c8ade
Santiago Torricella <torrisantiago@gmail.com>
2024-11-13 11:02:34 -0300

    Files

--- Taller de programacion II/Archivos/practicaArchivos/pracitica-05-11-2024.c
+++ Taller de programacion II/Archivos/practicaArchivos/pracitica-05-11-2024.c
@@ -10,15 +10,16 @@ int main()
 
     fputs("\nHola Mundo!",file);
 
- */
-
+*/
+/* 
+    ========= EJERCICO 2 ==========
     FILE *f;
     
     f = fopen("datos.txt","r");
 
     char miArchivo[100];
 
-    if(f == NULL)
+    if(f == NULL)  
     {
         printf("El archivo no se abrio bien");
     }
@@ -29,6 +30,63 @@ int main()
     }
 
     fclose(f);
+*/
+/* 
+    ======== EJERCICIO 3 ========
+    FILE *f;
+
+    f = fopen("datos.txt","r");
+    int contador = 0;
+
+    char miArchivo[100];
+
+    if(f == NULL)  
+    {
+        printf("El archivo no se abrio bien");
+    }
+
+     while(fgets(miArchivo,100,f))
+    {
+        contador ++;
+    }
+
+    printf("%d", contador);
+
+    fclose(f);
+*/
+/* 
+    ======== EJERCICIO 4 ========
+    FILE *f, *t;
+
+    f = fopen("source.txt", "r");
+    t = fopen("target.txt","w");
+    char miArchivo[100];
+
+    if(f == NULL) printf("El archivo fallo en abrirse");
+
+    while (fgets(miArchivo, 100, f))
+    {
+        fprintf(t,"%s",miArchivo); 
+    }
+    fclose(t);
+    t = fopen("target.txt","r");
+    while(fgets(miArchivo,100,t))   printf("%s", miArchivo);
+    fclose(f);
+ */
+
+    FILE *f;
+
+    f = fopen("texto.txt","r");
+
+    char miArchivo[100];
+
     
+    while (fgets(miArchivo,100,f))
+    {
+        printf("hola");
+    }
+    
+     
+
     return 0;
-}
+}  
