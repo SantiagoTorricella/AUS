b6ec3d5b81fe9d3d606860ca19fbd8d8f678cb57
Santiago <santiago@Santiagos-MacBook-Pro-2.local>
2025-09-22 12:06:14 -0300

    polish

--- 2 Año - 2 Cuatri/Taller de Programacion III/algolfait.java
+++ 2 Año - 2 Cuatri/Taller de Programacion III/algolfait.java
@@ -1,20 +1,21 @@
-public class A {
+class A {
     public A(){
         System.out.println("A");
     }
 }
 
 // Aunq extendamos de A, el constructor no se hereda
-public class B extends A { 
+class B extends A { 
     public B(){
-        System.out.prinln("B");
+        System.out.println("B");
     }
 }
 
 // Instancio un objeto de ambas clases
-
-A a = new A();
-B b = new B();
+// En java, todo codigo "ejectubal" se tiene que dar dentro de metodos, por eso comento lo de abajo
+// dado que no esta en ningun metodo de una clase, no puedo declarar esto en top-level
+// A a = new A();
+// B b = new B();
 
 // Si yo hago un a.toString() es decir un toString a un objeto
 // me va a devolver algo asi A@#3AF59B que es el nombre de la
